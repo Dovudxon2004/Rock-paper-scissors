b6d5c604d051a14f2748e21c2306610f9cc8cc29
Dovudxon2004 <68016631+Dovudxon2004@users.noreply.github.com>
2020-09-09 10:49:17 +0500

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,24 @@
+a='rock'
+b='scissors'
+c='paper'
+player1=input('Player1, your choice:')
+player2=input('Player2, your choice:')
+if player1 == a and player2 == b:
+    print('congratulations, player1')
+elif player1 == a and player2 == c:
+    print('congratulations, player2')
+elif player1 == b and player2 == a:
+    print('congratulations, player2')
+elif player1 == b and player2 == c:
+    print('congratulations, player1')
+elif player1 == c and player2 == a:
+    print('congratuations, player1')    
+elif player1 == c and player2 == b:
+    print('congratulations, player2')
+elif player1 == a and player2== a:
+    print('It was draw.')    
+elif player1 == b and player2 == b:
+    print('It was draw')
+elif player1 ==c and player2 == c:
+    print('It was draw')
+else: print("Either or both of you didn't type rock, paper, scissors correctly. Please, restart the game")
